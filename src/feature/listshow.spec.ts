14fb3661cecfedea966c4413161f74be80ff7988
Jan Pieter Posthuma <mail@datascenarios.nl>
2020-08-25 15:58:55 +0200

    update feature test

--- src/feature/listshow.spec.ts
+++ src/feature/listshow.spec.ts
@@ -32,7 +32,6 @@ import chaiAsPromise from "chai-as-promised";
 import { SinonStub } from "sinon";
 
 import { ModuleCommand } from "../lib/command";
-import * as parameters from "../lib/parameters";
 import * as api from "../lib/api";
 
 import { listshowAction } from "./listshow";
@@ -40,131 +39,49 @@ import { listshowAction } from "./listshow";
 chai.use(chaiAsPromise);
 const expect = chai.expect;
 
-describe("dataset/listshow.ts", () => {
-    let validateGroupIdMock: SinonStub<unknown[], unknown>;
-    let validateDatasetIdMock: SinonStub<unknown[], unknown>;
+describe("feature/listshow.ts", () => {
     let executeAPICallMock: SinonStub<unknown[], unknown>;
     const emptyOptions = {};
     const oneOptions = {
-        W: "c2a995d2-cd03-4b32-be5b-3bf93d211a56",
-    };
-    const allOptions = {
-        W: "c2a995d2-cd03-4b32-be5b-3bf93d211a56",
-        D: "datasetName",
+        feature: "feature",
     };
     const helpOptions = { H: true };
     beforeEach(() => {
-        validateGroupIdMock = ImportMock.mockFunction(parameters, "validateGroupId");
-        validateDatasetIdMock = ImportMock.mockFunction(parameters, "validateDatasetId");
         executeAPICallMock = ImportMock.mockFunction(api, "executeAPICall");
     });
     afterEach(() => {
-        validateGroupIdMock.restore();
-        validateDatasetIdMock.restore();
         executeAPICallMock.restore();
     });
     describe("listshowAction()", () => {
         it("list with --help", (done) => {
-            validateGroupIdMock.resolves(undefined);
-            validateDatasetIdMock.resolves(undefined);
             executeAPICallMock.resolves(true);
             const cmdOptsMock: unknown = {
                 name: () => "list",
                 opts: () => helpOptions,
             };
             listshowAction(cmdOptsMock as ModuleCommand).finally(() => {
-                expect(validateGroupIdMock.callCount).to.equal(0);
-                expect(validateDatasetIdMock.callCount).to.equal(0);
                 expect(executeAPICallMock.callCount).to.equal(0);
                 done();
             });
         });
         it("list with no options", (done) => {
-            validateGroupIdMock.resolves(undefined);
-            validateDatasetIdMock.resolves(undefined);
             executeAPICallMock.resolves(true);
             const cmdOptsMock: unknown = {
                 name: () => "list",
                 opts: () => emptyOptions,
             };
             listshowAction(cmdOptsMock as ModuleCommand).then(() => {
-                expect(validateGroupIdMock.callCount).to.equal(1);
-                expect(validateDatasetIdMock.callCount).to.equal(1);
                 expect(executeAPICallMock.callCount).to.equal(1);
                 done();
             });
         });
         it("list with one options", (done) => {
-            validateGroupIdMock.resolves(oneOptions.W);
-            validateDatasetIdMock.resolves(undefined);
             executeAPICallMock.resolves(true);
             const cmdOptsMock: unknown = {
                 name: () => "list",
                 opts: () => oneOptions,
             };
             listshowAction(cmdOptsMock as ModuleCommand).then(() => {
-                expect(validateGroupIdMock.callCount).to.equal(1);
-                expect(validateDatasetIdMock.callCount).to.equal(1);
-                expect(executeAPICallMock.callCount).to.equal(1);
-                done();
-            });
-        });
-        it("list with all options", (done) => {
-            validateGroupIdMock.resolves(allOptions.W);
-            validateDatasetIdMock.resolves(allOptions.D);
-            executeAPICallMock.resolves(true);
-            const cmdOptsMock: unknown = {
-                name: () => "list",
-                opts: () => allOptions,
-            };
-            listshowAction(cmdOptsMock as ModuleCommand).then(() => {
-                expect(validateGroupIdMock.callCount).to.equal(1);
-                expect(validateDatasetIdMock.callCount).to.equal(1);
-                expect(executeAPICallMock.callCount).to.equal(1);
-                done();
-            });
-        });
-        it("show with no options", (done) => {
-            validateGroupIdMock.rejects();
-            validateDatasetIdMock.rejects();
-            executeAPICallMock.resolves(true);
-            const cmdOptsMock: unknown = {
-                name: () => "show",
-                opts: () => emptyOptions,
-            };
-            listshowAction(cmdOptsMock as ModuleCommand).catch(() => {
-                expect(validateGroupIdMock.callCount).to.equal(1);
-                expect(validateDatasetIdMock.callCount).to.equal(0);
-                expect(executeAPICallMock.callCount).to.equal(0);
-                done();
-            });
-        });
-        it("show with one options", (done) => {
-            validateGroupIdMock.resolves(oneOptions.W);
-            validateDatasetIdMock.rejects();
-            executeAPICallMock.resolves(true);
-            const cmdOptsMock: unknown = {
-                name: () => "show",
-                opts: () => oneOptions,
-            };
-            listshowAction(cmdOptsMock as ModuleCommand).catch(() => {
-                expect(validateGroupIdMock.callCount).to.equal(1);
-                expect(validateDatasetIdMock.callCount).to.equal(1);
-                expect(executeAPICallMock.callCount).to.equal(0);
-                done();
-            });
-        });
-        it("show with all options", (done) => {
-            validateGroupIdMock.resolves(allOptions.W);
-            validateDatasetIdMock.resolves(allOptions.D);
-            executeAPICallMock.resolves(true);
-            const cmdOptsMock: unknown = {
-                name: () => "show",
-                opts: () => allOptions,
-            };
-            listshowAction(cmdOptsMock as ModuleCommand).then(() => {
-                expect(validateGroupIdMock.callCount).to.equal(1);
-                expect(validateDatasetIdMock.callCount).to.equal(1);
                 expect(executeAPICallMock.callCount).to.equal(1);
                 done();
             });
