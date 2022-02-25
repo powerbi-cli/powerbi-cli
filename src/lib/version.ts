1f8a5e757f3313f7d2224fae54e0d849ad63f0f6
Jan Pieter Posthuma <mail@datascenarios.nl>
2022-02-25 14:59:22 +0100

    dump version v1.2.1

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -32,7 +32,7 @@ import { yellow } from "chalk";
 
 import { verbose } from "./logging";
 
-export const currentVersion = "1.2.0";
+export const currentVersion = "1.2.1";
 const versionUrl = "https://powerbicli.azureedge.net/version.json";
 
 interface version {
